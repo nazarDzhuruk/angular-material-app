de83198476e096122b337be125b6ed3923811f53
Nazar Dzhuruk <nazar.dzhuruk@soprasteria.com>
2023-03-29 14:52:05 +0200

    Add dialog and popup

---
+++ src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-new-contact-dialog',
+  templateUrl: './new-contact-dialog.component.html',
+  styleUrls: ['./new-contact-dialog.component.scss']
+})
+export class NewContactDialogComponent implements OnInit {
+  avatars = ["svg-1", "svg-2", "svg-3", "svg-4", "svg-12"];
+  user: User | any;
+  name = new FormControl('', [Validators.required]);
+
+  constructor(private dialogRef: MatDialogRef<NewContactDialogComponent>, private service: UserService) { }
+
+  ngOnInit(): void {
+    this.user = new User();
+  }
+
+  save(): void {
+    this.user.name = this.name.value;
+    this.service.addUser(this.user).then(user => {
+      this.dialogRef.close(user);
+    });
+    
+  }
+
+  dismiss(): void {
+    this.dialogRef.close(null);
+  }
+
+  getErrorMessage() {
+    return this.name.hasError('required') ? 'You must enter a value' : '';
+  }
+
+}
--- src/app/contactmanager/services/user.service.ts
+++ src/app/contactmanager/services/user.service.ts
@@ -37,4 +37,13 @@ export class UserService {
       console.log("Failed to fetch");
     })
   }
+
+  addUser(user: User): Promise<User>{
+    return new Promise((resolver, reject) => {
+      user.id = this.dataStore.users.length + 1;
+      this.dataStore.users.push(user);
+      this._users.next(Object.assign({}, this.dataStore).users);
+      resolver(user);
+    });
+  }
 }
